abdc99434aabeb10206ce9a26650e8c94e858673
martin-creator <martinlubowa@outlook.com>
2023-09-28 00:03:39 +0300

    Created accounts app

--- social_backend/social_backend/settings.py
+++ social_backend/social_backend/settings.py
@@ -62,8 +62,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'djangorestframework',
-    'djangorestframework_simplejwt',
+    'rest_framework',
+    'rest_framework_simplejwt',
     'corsheaders',
 ]
 
