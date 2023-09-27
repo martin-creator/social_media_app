85455a97a8f16bc4205b09afbee8ea5ee232bcdc
martin-creator <martinlubowa@outlook.com>
2023-09-27 21:06:34 +0300

    Created people you may know component

---
+++ social_frontend/src/components/PeopleYouMayKnow.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="space-y-4">
+    <div class="flex items-center justify-between">
+      <div class="flex items-center space-x-2">
+        <img
+          src="https://i.pravatar.cc/300?img=70"
+          class="w-[40px] rounded-full"
+        />
+
+        <p class="text-xs"><strong>Code With Stein</strong></p>
+      </div>
+
+      <a href="#" class="py-2 px-3 bg-purple-600 text-white text-xs rounded-lg"
+        >Show</a
+      >
+    </div>
+
+    <div class="flex items-center justify-between">
+      <div class="flex items-center space-x-2">
+        <img
+          src="https://i.pravatar.cc/300?img=70"
+          class="w-[40px] rounded-full"
+        />
+
+        <p class="text-xs"><strong>Code With Stein</strong></p>
+      </div>
+
+      <a href="#" class="py-2 px-3 bg-purple-600 text-white text-xs rounded-lg"
+        >Show</a
+      >
+    </div>
+
+    <div class="flex items-center justify-between">
+      <div class="flex items-center space-x-2">
+        <img
+          src="https://i.pravatar.cc/300?img=70"
+          class="w-[40px] rounded-full"
+        />
+
+        <p class="text-xs"><strong>Code With Stein</strong></p>
+      </div>
+
+      <a href="#" class="py-2 px-3 bg-purple-600 text-white text-xs rounded-lg"
+        >Show</a
+      >
+    </div>
+  </div>
+</template>
